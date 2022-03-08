ab9bac0a892fd7923fa7a6ed81f6b873f27f7f36
Pablo Muñoz <pjmd89@gmail.com>
2022-03-08 04:34:59 -0400

    fix bug

--- main.go
+++ main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"github.com/pjmd89/gogql/lib/gql"
 	"github.com/pjmd89/gogql/lib/http"
-	"github.com/pjmd89/gogql/resolvers"
 )
 
 func main(){
 
 	schema := gql.Init("localhost","schema");
-	schema.ObjectType("Provider",resolvers.NewProvider());
+	//schema.ObjectType("Provider",resolvers.NewProvider());
 	myHttp := http.Init(schema);
 	myHttp.Start();
 
