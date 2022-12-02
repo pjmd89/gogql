d014b0abdeb1d48f8daa0e6be9c0007e8c2749b4
Pablo <pjmd89@gmail.com>
2022-12-02 13:54:53 -0400

    up

--- lib/resolvers/restinfo.go
+++ lib/resolvers/restinfo.go
@@ -5,7 +5,7 @@ import "net/http"
 func (o *RestInfo) SetHTTPRequest(r *http.Request) {
 	o.r = r
 }
-func (o *RestInfo) SetHeeader(header, value string) {
+func (o *RestInfo) SetHeader(header, value string) {
 	if len(o.headers) == 0 {
 		o.headers = make(map[string]string)
 	}
